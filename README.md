72005a0c60589e94467fe31d7774740e962c8ebb
Andreas Schlagner <andreas-schlagner_student2022@wilder.school>
2022-03-17 20:42:08 +0100

    Output of formatted date and weekday number

---
+++ README.md
@@ -0,0 +1,27 @@
+# JavaQuest - The dates
+SEA8 - Quest: The dates
+
+Challenge: Date formatting
+
+In order to practice creating dates, first of all make a Fork of the following deposit then clone it locally.
+
+Be sure to do a _Fork_ or you won't be able to grow anything !
+
+    1. Once the project is locally recovered, open the fileTestDate.java
+    2. You will see that it already contains code to retrieve three data: a day, a month and a year.
+    3. Use this data and your knowledge gained in the quest to calculate the following information:
+
+A date formatted as "month-day-year", ex: 04-22-2019 The day of the week corresponding to the date, ex: 2
+
+Concatenates texts _"The date is: "_ and _"The day of week is:"_ in front of the corresponding data, before displaying them in the terminal.
+
+Expected outcome :
+
+$ Please fill a day (1-31) :
+$ 22
+$ Please fill a month (1-12) :
+$ 4
+$ Please fill a year :
+$ 2019
+$ The date is : 04-22-2019
+$ The day of week is : 2
--- TestDate.java
+++ TestDate.java
@@ -2,7 +2,8 @@ import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
 import java.util.Date;
-import java.util.Scanner; 
+import java.util.Scanner;
+import java.text.ParseException;
 
 class TestDate {
 
@@ -16,8 +17,25 @@ class TestDate {
         System.out.println("Please fill a year :");
         int year = sc.nextInt();
         sc.close();
+        // int day = 22;
+        // int month = 4;
+        // int year = 2019;
 
         // TODO : your code after this line
-        
+        SimpleDateFormat sdf = new SimpleDateFormat("MM-dd-yyyy");
+        try {
+        Date date = sdf.parse(month + "-" + day + "-" + year);
+        System.out.println("The date is : " + sdf.format(date));
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+         
+        Calendar calendar = new GregorianCalendar();
+        calendar.set(Calendar.YEAR, year);
+        calendar.set(Calendar.MONTH, month - 1);  // Months start at 0 (Jan = 0, Dec = 11) 
+        calendar.set(Calendar.DAY_OF_MONTH, day);
+        int dayOfWeek = calendar.get(Calendar.DAY_OF_WEEK);
+        System.out.println("The day of week is : " + dayOfWeek);
+
     }
 }
